80736d9ccca8c67b601fd992e1f38b69ccdf35aa
Ankur Goel <ankurgel@gmail.com>
2013-07-14 19:14:45 +0530

    working on AR(p) and MA(q) simulators

--- lib/statsample/arima.rb
+++ lib/statsample/arima.rb
@@ -23,6 +23,27 @@ module Statsample
 
       def yule_walker()
       end
+
+      #tentative AR(p) simulator
+      def ar_sim(n, phi, sigma)
+        #using random number generator for inclusion of white noise
+        err_nor = Distribution::Normal.rng(0, sigma)
+
+        a = Array.new(n, 0)
+
+        #For now "phi" are the known model parameters
+        #later we will obtain it by Yule-walker/Burg
+
+        1.upto(n) do |i|
+          summation = 0
+          phi.each_with_index do |phi_i, j|
+            summation += phi_i * x[i - j - 1]
+          end
+          x[i] = summation + err_nor.call()
+        end
+        x
+      end
+
     end
   end
 end
